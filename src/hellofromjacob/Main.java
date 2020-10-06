db4427d6e7eedd480b5477b39014b8a84a2a1aa4
JacobDeMille <jacob.demille@icloud.com>
2020-10-06 11:15:38 -0600

    Added two more lines for the stretch challenge

--- src/hellofromjacob/Main.java
+++ src/hellofromjacob/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	System.out.println("Hello my name is Jacob");
-	System.out.println("This is a comment for the stretch challenge!");
+    System.out.println("This is a comment for the stretch challenge!");
+    //Another line of code for the stretch challenge
+    System.out.println("A third line of code for the strech challenge!");
     }
 }
