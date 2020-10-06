57a46156b99ee8c0e71840f7c968d6ab4e475807
JacobDeMille <jacob.demille@icloud.com>
2020-10-06 11:23:04 -0600

    Removed lines of code with a new commit for stretch challenge

--- src/hellofromjacob/Main.java
+++ src/hellofromjacob/Main.java
@@ -5,7 +5,5 @@ public class Main {
     public static void main(String[] args) {
 	System.out.println("Hello my name is Jacob");
     System.out.println("This is a comment for the stretch challenge!");
-    //Another line of code for the stretch challenge
-    System.out.println("A third line of code for the strech challenge!");
     }
 }
