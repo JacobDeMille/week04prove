7bfa738d384625cc76afa8961e7fe33281f99184
Valerie Ludden <valudden@gmail.com>
2020-10-06 17:57:54 -0600

    Valerie's commit

--- src/hellofromjacob/Main.java
+++ src/hellofromjacob/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	System.out.println("Hello my name is Jacob");
     System.out.println("This is a comment for the stretch challenge!");
+    System.out.println("Hi Jacob. This is Valerie.");
     }
 }
